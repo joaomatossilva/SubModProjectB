12303eaf99dd63b0a87285cec31ae9f323f70364
João Filipe Matos Silva <joao@kspace.pt>
2019-01-07 22:53:00 +0000

    some project code

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SubModCommon;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = Enum.GetValues(typeof(TestEnum));
+            foreach (var value in values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
